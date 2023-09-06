8dfbc52aecc4408940b1a11564aa2f4ab6dfb48c
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-09-06 18:50:15 -0500

    added logout button to dashboard

---
+++ src/app/dashboard/logout-button.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { Database } from '../../types/database.types'
+import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+
+export default function LogoutButton({ session }: { session: Session | null }) {  
+  return (
+    <div className="form-widget">
+      <div>
+        <form action="/auth/signout" method="post">
+          <button className="button block" type="submit">
+            Sign out
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
