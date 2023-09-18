ba78377bdcdcb28c4b240300bfbc7a196b53d2df
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-09-18 12:36:03 -0500

    Added auth form component

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -1,22 +1,104 @@
 'use client'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { useTheme } from "next-themes"
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import * as React from "react"
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from "zod"
 
-export default function AuthForm() {
-  const supabase = createClientComponentClient<Database>();
-  const { theme } = useTheme();
+import { cn } from "@/lib/utils"
+import { userAuthSchema } from "@/lib/validations/auth"
+import { Icons } from "@/components/icons"
+import { buttonVariants } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { toast } from "@/components/ui/use-toast"
 
+interface AuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+type FormData = z.infer<typeof userAuthSchema>
+
+export function AuthForm({ className, ...props }: AuthFormProps) {
+  const { register, handleSubmit, formState: {errors} } = useForm<FormData>({
+    resolver: zodResolver(userAuthSchema),
+  })
+
+  const [isLoading, setIsLoading] = React.useState<boolean>(false)
+
+  async function onSubmit(data: FormData) {
+    setIsLoading(true)
+
+    // Send a POST request to the auth/signin NextJS route
+    const res = await fetch('/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    setIsLoading(false)
+
+    // If an error occurred, display it in a toast
+    if (res.status === 401) {
+      const error = await res.text();
+      console.error(error);
+      return toast({
+        title: "Something went wrong.",
+        description: "Your sign in request failed. Please try again.",
+        variant: "destructive",
+      })
+    }
+  }
   return (
-    <Auth
-      supabaseClient={supabase}
-      view="magic_link"
-      appearance={{ theme: ThemeSupa }}
-      theme={theme === 'dark' || theme === 'system' ? 'dark' : 'light'}
-      showLinks={false}
-      providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
-    />
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+              {...register("email")}
+            />
+            {errors?.email && (
+              <p className="px-1 text-xs text-red-600">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Password
+            </Label>
+            <Input
+              id="password"
+              placeholder="********"
+              type="password"
+              autoCapitalize="none"
+              autoComplete="current-password"
+              autoCorrect="off"
+              disabled={isLoading}
+              {...register("password")}
+            />
+            {errors?.password && (
+              <p className="px-1 text-xs text-red-600">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+          <button className={cn(buttonVariants())} disabled={isLoading}>
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
