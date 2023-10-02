19694298b413a392361faa04863a70bfd2e6090b
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-10-01 22:26:43 -0500

    Created nav-bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './styles/globals.css'
 import { ThemeProvider } from "@/components/theme-provider"
-import { ModeToggle } from "@/components/mode-toggle"
+import { Toaster } from "@/components/ui/toaster"
+import { NavBar } from '@/components/nav-bar'
 
 export default function RootLayout({
     children,
@@ -8,16 +9,16 @@ export default function RootLayout({
     children: React.ReactNode
   }) {
     return (
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <head>
           <title>RPS Survey</title>
         </head>
-        <body>
+        <body className="min-h-screen flex flex-col bg-background font-sans antialiased">
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="flex justify-end">
-              <ModeToggle />
-            </div>
-            {children}
+            <NavBar />
+            <div className="curved-gradient"></div>
+              {children}
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
