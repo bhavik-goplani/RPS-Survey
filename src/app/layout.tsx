574f2cc48ee787ff8e70c62e361a22baf38f3dcf
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-10-03 10:41:49 -0500

    Get session for user to render UI dynamically

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,19 @@ import './styles/globals.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/toaster"
 import { NavBar } from '@/components/nav-bar'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
   }: {
     children: React.ReactNode
   }) {
+    const supabase = createServerComponentClient<Database>({ cookies })
+    const {
+        data: { session },
+    } = await supabase.auth.getSession()
+
     return (
       <html lang="en" suppressHydrationWarning>
         <head>
@@ -15,7 +22,7 @@ export default function RootLayout({
         </head>
         <body className="min-h-screen flex flex-col bg-background font-sans antialiased">
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <NavBar />
+            <NavBar session={session}/>
             <div className="curved-gradient"></div>
               {children}
             <Toaster />
