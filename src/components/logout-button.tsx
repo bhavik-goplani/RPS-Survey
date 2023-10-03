7f58852b182b204fab96ca994ddfb9b3d9d43563
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-10-03 10:36:21 -0500

    Renders button on navbar when user signs in

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -1,11 +1,15 @@
-import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { Session } from '@supabase/auth-helpers-nextjs'
+import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 
 export default function LogoutButton({ session }: { session: Session | null }) {  
+
+  if (!session) return (<></>)
   return (
     <div className="form-widget">
       <div>
         <form action="/auth/signout" method="post">
-          <button className="button block" type="submit">
+          <button className={cn(buttonVariants())} type="submit">
             Sign out
           </button>
         </form>
