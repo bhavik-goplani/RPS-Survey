418ca7044480d28a81d8bca7bde4bb2ec144cbc1
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-12-30 22:26:42 +0400

    column definitions for survey table

--- src/components/survey-dashboard/columns.tsx
+++ src/components/survey-dashboard/columns.tsx
@@ -1,7 +1,17 @@
 'use client'
 
 import { ColumnDef } from "@tanstack/react-table"
-import { UUID } from "crypto"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -23,6 +33,45 @@ export const columns: ColumnDef<Survey>[] = [
   },
   {
     accessorKey: "section_count",
-    header: "Section Count",
+    header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Section Count
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const survey = row.original
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(survey.survey_id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
   },
 ]
