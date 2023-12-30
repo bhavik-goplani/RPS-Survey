871b624e1c35650539aab44fa5cfb3151e8229a5
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-12-30 22:24:41 +0400

    added table component to page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,8 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import { Survey, columns } from '@/components/survey-dashboard/columns'
+import { DataTable } from '@/components/ui/data-table'
+
 
 export default async function Home() {
     const supabase = createServerComponentClient<Database>({ cookies })
@@ -8,7 +11,21 @@ export default async function Home() {
         data: { session },
     } = await supabase.auth.getSession()
     
+    async function getSurveys() : Promise<Survey[]> {        
+        let { data: Survey, error } = await supabase
+        .from('Survey')
+        .select('*')
+        console.log(Survey)
+        if (error) console.log('error', error)
+        return Survey as Survey[]
+    }
+
+    const data = await getSurveys()
     return (
-        <></>
+        <>
+            <div className="container mx-auto py-10">
+                <DataTable columns={columns} data={data} />
+            </div>
+        </>
     )
   }
