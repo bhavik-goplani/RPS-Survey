8d210d89b426cc676d2b70f8dcc427675f5a772c
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-02-07 18:28:10 -0600

    Handle last section logic

--- src/components/survey/section.tsx
+++ src/components/survey/section.tsx
@@ -1,13 +1,33 @@
 'use client'
 import { Button } from "@/components/ui/button"
 
-export function Section({ section_id, onComplete }: { section_id: string, onComplete: () => void }) {
+export function Section({ section_id, onComplete, isLastSection }: { section_id: string, onComplete: () => void, isLastSection: boolean}) {
 
 
     function handleComplete() {
-        onComplete()
+        if (isLastSection) {
+            console.log('Survey Complete')
+        }
+        else{
+            onComplete()
+        }
     }
-    
+
+        if (isLastSection) {
+            return (
+                <>
+                    <div className="container mx-auto py-10">
+                        <div className='flex justify-between'>
+                            <br />
+                            <h1 className="text-2xl font-semibold tracking-tight">Section - {section_id}</h1>
+                            <Button onClick={handleComplete}>
+                                Complete Survey
+                            </Button>
+                        </div>
+                    </div>
+                </>
+            )
+        }
         return (
             <>
                 <div className="container mx-auto py-10">
